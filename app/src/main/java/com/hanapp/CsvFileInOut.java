567a1e02f31a5c7716bfe615a990b1837510e0c5
chesterkyles <chesterkylescolita07@gmail.com>
2019-07-21 08:27:49 +0800

    OCR

--- app/src/main/java/com/hanapp/CsvFileInOut.java
+++ app/src/main/java/com/hanapp/CsvFileInOut.java
@@ -156,8 +156,8 @@ public class CsvFileInOut {
                 place_name.add(row[3]);
                 price.add(row[4]);
                 path.add(row[5]);
-                location_latitude.add(row[6]);
-                location_longitude.add(row[7]);
+                //location_latitude.add(row[6]);
+                //location_longitude.add(row[7]);
                 index += 1;
             }
             ArrayList<String> index_str = new ArrayList<>();
